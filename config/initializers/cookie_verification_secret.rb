7371ddd7ef236cbac2e58e3a0c670b89e0099623
Matheus Tardivo <matheustardivo@gmail.com>
2010-07-04 21:23:40 -0300

    Atualização na configuração do banco de dados Comentado ActionController::Base.cookie_verifier_secret do arquivo cookie_verification_secret.rb Atualizações no banco de dados (remover esses arquivos e também os logs do versionamento e também adicionar no ignore) Removido arquivo restart.txt para reiniciar o passenger

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '5bf561c52fba8dd2c4f872125927026f03445bc5b563e5fc9f39155c1a792d09f81fbb01cb4f14a2f5b6a59a0fb900502a2dce1cad3a6399dad1342a0edc8e1a';
+# ActionController::Base.cookie_verifier_secret = '5bf561c52fba8dd2c4f872125927026f03445bc5b563e5fc9f39155c1a792d09f81fbb01cb4f14a2f5b6a59a0fb900502a2dce1cad3a6399dad1342a0edc8e1a';
