f73282aaae57c1df57563bdf7c698efcb95db00d
Matheus Tardivo <matheustardivo@gmail.com>
2010-07-13 20:50:36 -0300

    Inclusão de verificação de campos em branco

--- app/models/despesa.rb
+++ app/models/despesa.rb
@@ -1,5 +1,7 @@
 class Despesa < ActiveRecord::Base
   
+  validates_presence_of :data, :tipo_id, :parcelas, :valor
+  
   belongs_to :fatura
   belongs_to :tipo
 end
--- app/models/pagamento.rb
+++ app/models/pagamento.rb
@@ -1,5 +1,7 @@
 class Pagamento < ActiveRecord::Base
   
+  validates_presence_of :nome, :vencimento, :valor
+  
   belongs_to :user
   belongs_to :agenda
   
