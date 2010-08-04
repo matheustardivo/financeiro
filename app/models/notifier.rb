56279e18d4902346a3d970087325430eb0c32e80
Matheus Tardivo <matheustardivo@gmail.com>
2010-08-03 23:33:05 -0300

    Correção para enviar os emails em produção

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
 
   def password_reset_instructions(user)
     subject "Instruções para resetar sua senha"
-    from "Tardivo.org - Financeiro <noreply@tardivo.org>"
+    from "noreply@tardivo.org"
     recipients user.email
     sent_on Time.now
     body :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
