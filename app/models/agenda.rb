4fe8cdadfe456fbb061757c44b1c3acdfda0bd20
Matheus Tardivo <matheustardivo@gmail.com>
2011-05-25 08:00:35 -0300

    Validação nome e mês únicos, inclusão do valor da fatura nos valores da agenda e método para obter as faturas relacionadas a agenda atual

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,6 +1,7 @@
 class Agenda < ActiveRecord::Base
   
   validates_presence_of :nome, :mes
+  validates_uniqueness_of :nome, :mes
   
   belongs_to :user
   has_many :pagamentos, :order => "vencimento"
@@ -10,6 +11,10 @@ class Agenda < ActiveRecord::Base
     pagamentos.each do |pagamento|
       valor += pagamento.valor if pagamento.confirmado
     end
+    
+    faturas.each do |fatura|
+      valor += fatura.saldo if fatura.confirmado
+    end
     return Dinheiro.new(valor).real_formatado
   end
   
@@ -17,6 +22,10 @@ class Agenda < ActiveRecord::Base
     valor = 0
     pagamentos.each do |pagamento|
       valor += pagamento.valor unless pagamento.confirmado
+    end
+    
+    faturas.each do |fatura|
+      valor += fatura.saldo unless fatura.confirmado
     end
     return Dinheiro.new(valor).real_formatado
   end
@@ -35,5 +44,13 @@ class Agenda < ActiveRecord::Base
   
   def mes_formatado=(mes_atual)
     write_attribute(:mes, Date.strptime(mes_atual, "%m/%Y"))
+  end
+  
+  def faturas
+    nil unless :mes
+    
+    Fatura.where("faturas.mes = ? and cartaos.user_id = ?", self.mes, self.user_id).
+      joins("inner join cartaos on faturas.cartao_id = cartaos.id").
+      order("cartaos.descricao")
   end
 end
