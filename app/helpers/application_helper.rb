b5e6aebdb0190be8990f526d0546682c35f9dcb3
Matheus Tardivo <matheustardivo@gmail.com>
2011-05-25 07:58:59 -0300

    Métodos para exibir os links de confirmação e cancelamento do pagamento de faturas e pagamentos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  
+  def pagamento_tag(pagamento)
+    if pagamento.confirmado
+			link_para('Sim', 'pagamentos', 'Cancelar', pagamento.id)
+		else
+			link_para('Não', 'pagamentos', 'Confirmar', pagamento.id)
+		end
+  end
+  
+  def fatura_tag(fatura)
+    return "Não" unless fatura.situacao == Fatura::SITUACAO[:fechada]
+    if fatura.confirmado
+  			link_para('Sim', 'faturas', 'Cancelar', fatura.id)
+  		else
+  			link_para('Não', 'faturas', 'Confirmar', fatura.id)
+		end
+  end
+  
+  private
+  
+    def link_para(label, controller, action, id)
+      link_to label, { :controller => controller, :action => action.downcase, :id => id }, :confirm => "#{action} o pagamento?", :method => :put, :remote => true
+    end
 end
